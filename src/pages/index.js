158184bc22a296c7cd0f168ac48dc983244982c5
Josh Downs <jdowns@burlingtoncodeacademy.com>
2020-09-27 17:49:59 -0400

    fixed import

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import usePosts from "../hooks/use-posts";
 import Layout from "../components/layout";
 import PostPreview from "../components/post-preview";
 import Hero from "../components/hero";
-import Insta from "../components/Insta"
+import Insta from "../components/insta";
 
 export default () => {
   const posts = usePosts();
